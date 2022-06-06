d3c2679d2b30c1410b3af9d8d09e8d3b7ec78996
Kumiko Randle <kcrandle98@hotmail.com>
2022-06-06 16:02:08 -0400

    Updated database
    Updated database to include all technologies

--- src/technology/admin.py
+++ src/technology/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Technology
 
+class TechnologyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type')
+
 # Register your models here.
-admin.site.register(Technology)
+admin.site.register(Technology, TechnologyAdmin)
